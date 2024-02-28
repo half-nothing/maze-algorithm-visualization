20238bbb6cb2ab481ceb6b92837cd82746d4c542
Half_nothing <Half_nothing@163.com>
2024-02-28 23:25:45 +0800

    初始化QT

---
+++ src/Qt/include/MainWindow.h
@@ -0,0 +1,24 @@
+#ifndef DAC_MAINWINDOW_H
+#define DAC_MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace QT {
+    QT_BEGIN_NAMESPACE
+    namespace Ui { class MainWindow; }
+    QT_END_NAMESPACE
+
+    class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+    public:
+        explicit MainWindow(QWidget *parent = nullptr);
+
+        ~MainWindow() override;
+
+    private:
+        Ui::MainWindow *ui;
+    };
+}
+
+#endif
---
+++ src/Qt/src/MainWindow.cpp
@@ -0,0 +1,14 @@
+#include "ui_MainWindow.h"
+#include "../include/MainWindow.h"
+
+
+namespace QT {
+    MainWindow::MainWindow(QWidget *parent) :
+            QMainWindow(parent), ui(new Ui::MainWindow) {
+        ui->setupUi(this);
+    }
+
+    MainWindow::~MainWindow() {
+        delete ui;
+    }
+}
